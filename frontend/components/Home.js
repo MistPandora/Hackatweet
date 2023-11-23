e531093bbbe8d6b0188060b67049c887f2e061bf
sébastien <sebastien.faure24@orange.fr>
2023-11-23 12:50:30 +0100

    Hackaf

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -2,43 +2,12 @@ import styles from '../styles/Home.module.css';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import SignUp from './signup';
-
+import { Popover, Button } from 'antd';
 function Home() {
   const dispatch = useDispatch();
   //const user = useSelector((state) => state.user.value);
 
-  const handleRegister = () => {
-    fetch('http://localhost:3000/users/signup', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ firstName: signUpFirstname, username: signUpUsername, password: signUpPassword }),
-    }).then(response => response.json())
-      .then(data => {
-        if (data.result) {
-          dispatch(login({ username: signUpUsername, token: data.token }));
-          setSignUpUsername('');
-          setSignUpPassword('');
-          setIsModalVisible(false)
-        }
-      });
-  };
 
-  const handleConnection = () => {
-
-    fetch('http://localhost:3000/users/signin', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ firstName: signInFirstname, username: signInUsername, password: signInPassword }),
-    }).then(response => response.json())
-      .then(data => {
-        if (data.result) {
-          dispatch(login({ username: signInUsername, token: data.token }));
-          setSignInUsername('');
-          setSignInPassword('');
-          setIsModalVisible(false)
-        }
-      });
-  };
 
 
 
@@ -49,7 +18,8 @@ function Home() {
     <div className={styles.container}>
 
       <Popover title="Sign Up" content={<SignUp />} className={styles.popover} trigger="click">
-        <Button>Sign Up</Button>
+        <Button>Sign up</Button>
+        <Button>Sign in</Button>
       </Popover>
 
 
--- frontend/components/signin.js
+++ frontend/components/signin.js
@@ -1,8 +1,53 @@
 import styles from '../styles/Home.module.css';
-
+import Image from 'next/Image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 
 
 function signin() {
 
-}
+    const handleConnection = () => {
+
+        fetch('http://localhost:3000/users/signin', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username: signInUsername, password: signInPassword }),
+        }).then(response => response.json())
+            .then(data => {
+                if (data.result) {
+                    dispatch(login({ username: signInUsername, token: data.token }));
+                    setSignInUsername('');
+                    setSignInPassword('');
+                    setIsModalVisible(false)
+                }
+            });
+    };
+
+
+
+
+
+
+
+    return (
+        <div className={styles.container}>
+            <FontAwesomeIcon icon={faXmark} />
+            <div className={styles.header}>
+                <div className={styles.logocontainer}>
+                    <Image src="../logoTwitter" alt="Logo" width={100} height={100} />
+                    <p className={styles.signTitle}>Connect to Hackatweet </p>
+                </div>
+            </div>
+            <div className={styles.signSection}>
+                <input type="text" placeholder="Username" id="signInUsername" onChange={(e) => (e.target.value)} />
+                <input type="password" placeholder="Password" id="signInPassword" onChange={(e) => (e.target.value)} />
+                <button id="connection" onClick={() => handleConnection()}>Sign in</button>
+            </div>
+
+        </div>
+    )
+};
+
+
+
