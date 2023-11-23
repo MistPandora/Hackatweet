9d41f8f653d77b1d1d13051ffcc18cbfb8333bf2
sébastien <sebastien.faure24@orange.fr>
2023-11-23 11:22:51 +0100

    seb first commit

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,16 +1,46 @@
 import styles from '../styles/Home.module.css';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function Home() {
-  const activities = [
-    {
-      name: 'SPORT',
-      timer: 495,
-    },
-    {
-      name: 'WORK',
-      timer: 2074,
-    },
-  ];
+  const dispatch = useDispatch();
+  //const user = useSelector((state) => state.user.value);
+
+  const handleRegister = () => {
+    fetch('http://localhost:3000/users/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ firstName: signUpFirstname, username: signUpUsername, password: signUpPassword }),
+    }).then(response => response.json())
+      .then(data => {
+        if (data.result) {
+          dispatch(login({ username: signUpUsername, token: data.token }));
+          setSignUpUsername('');
+          setSignUpPassword('');
+          setIsModalVisible(false)
+        }
+      });
+  };
+
+  const handleConnection = () => {
+
+    fetch('http://localhost:3000/users/signin', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ firstName: signInFirstname, username: signInUsername, password: signInPassword }),
+    }).then(response => response.json())
+      .then(data => {
+        if (data.result) {
+          dispatch(login({ username: signInUsername, token: data.token }));
+          setSignInUsername('');
+          setSignInPassword('');
+          setIsModalVisible(false)
+        }
+      });
+  };
+
+
+
 
   const activitiesComponents = activities.map((data, i) => {
     return <Activity key={i} name={data.name} timer={data.timer} />;
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,14 +1,34 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+// import user from '../reducers/user'
+
+
+const reducers = combineReducers({});
+const persistConfig = { key: 'tweet', storage };
+
+const store = configureStore({
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+});
+
+const persistor = persistStore(store);
+
 
 function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <title>My Project</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Head>
+          <title>My Project</title>
+        </Head>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
   );
 }
 
